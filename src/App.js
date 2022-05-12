bcdf21c16f1d9fd34a2474604bfcbb3966b2c4c4
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-12 13:22:20 +0600

    added pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login';
 import Navbar from './Pages/Navbar/Navbar';
+import Reviews from './Pages/Reviews/Reviews';
 
 
 function App() {
   return (
     <div className="App">
      <Navbar></Navbar>
-    <Home></Home>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/home' element={<Home></Home>}></Route>
+      <Route path='/about' element={<About></About>}></Route>
+      <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+      <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+      <Route path='/login' element={<Login></Login>}></Route>
+    </Routes>
     </div>
   );
 }
